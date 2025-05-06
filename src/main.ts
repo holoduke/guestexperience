a57edcb02e1f4a8689ab4f378d7be36f3780bc74
gillis@newstory.nl <gillis@newstory.nl>
2025-05-06 14:05:16 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,11 @@ platformBrowserDynamic()
 /* ------------------------------------------
    🔑  Only register the SW in production
    ------------------------------------------ */
+   console.log("going to register service worker?");
    if (environment.production && 'serviceWorker' in navigator) {
     // Wait until the app is stable or the browser finishes loading
     window.addEventListener('load', () => {
+      console.log("yes registering service worker");
       navigator.serviceWorker.register('/service-worker.js')
         .then(reg => console.log('[SW] registered:', reg))
         .catch(err => console.error('[SW] registration failed:', err));
